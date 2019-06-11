c858ad2821c926f3dab9086f860da6c32ba610c1
Josh Armentano <josh@abidewebdesign.com>
2019-06-11 13:59:03 -0700

    Added ACF JSON. Added features to block - call out.

--- template-parts/blocks/block-call-out.php
+++ template-parts/blocks/block-call-out.php
@@ -4,20 +4,37 @@
 
 		<div class="row justify-content-center">
 			
-			<div class="col-md-7 col-lg-6">
-				
-				<h2 class="text-white"><?php the_sub_field('call_out_text'); ?></h2>
-				
-			</div>
+			<?php if ( get_sub_field('call_out_type') == 'default' ): ?>
 			
-			<div class="col-md-5 col-lg-4 align-self-center">
+				<div class="col-md-7 col-lg-6">
+					
+					<h2 class="text-white"><?php the_sub_field('call_out_text'); ?></h2>
+					
+				</div>
 				
+				<div class="col-md-5 col-lg-4 align-self-center">
+					
+					<?php $link = get_sub_field('call_out_button_link'); ?>
+					
+					<a href="<?php echo $link['url']; ?>" class="btn btn-lg btn-light" target="<?php echo $link['target']; ?>"><?php the_sub_field('call_out_button_label'); ?></a>
+					
+				</div>
 				
-				<?php $link = get_sub_field('call_out_button_link'); ?>
-				
-				<a href="<?php echo $link['url']; ?>" class="btn btn-lg btn-light" target="<?php echo $link['target']; ?>"><?php the_sub_field('call_out_button_label'); ?></a>
-				
-			</div>
+			<?php else: ?>
+			
+				<div class="col-lg-10">
+					
+					<h3 class="text-white"><?php the_sub_field('call_out_text'); ?></h3>
+					
+					<?php if ( get_sub_field('call_out_quote_name') ): ?>
+						
+						<p class="text-white"><strong>- <?php the_sub_field('call_out_quote_name'); ?></strong></p>
+						
+					<?php endif; ?>
+					
+				</div>
+			
+			<?php endif; ?>
 			
 		</div>
 		
